41017803118558655ff016b546fe83636b849788
JainalArifin <zainalunpam05@gmail.com>
2018-07-17 17:40:20 +0700

    challenge class done about make calculator

---
+++ class.js
@@ -0,0 +1,42 @@
+class Kalkulator {
+    constructor(angka1, angka2){
+        this.angka1 = angka1,
+        this.angka2 = angka2,
+        this.hasil = 0
+    }
+
+    tambah(){
+        this.hasil = this.angka1 + this.angka2
+        return this.hasil
+    }
+
+    kurang(){
+        this.hasil = this.angka1 - this.angka2
+        return this.hasil
+    }
+
+    kali(){
+        this.hasil = this.angka1 * this.angka2
+        return this.hasil
+    }
+
+    bagi(){
+        this.hasil = this.angka1 / this.angka2
+        return this.hasil
+    }
+}
+
+const angka1 = 2
+const angka2 = 3
+const kalkulator = new Kalkulator(angka1, angka2)
+
+kalkulator.tambah()
+console.log(kalkulator.hasil) // 5
+kalkulator.kurang()
+console.log(kalkulator.hasil) // -1
+kalkulator.kali()
+console.log(kalkulator.hasil) // 6
+kalkulator.angka1 = 6
+kalkulator.angka2 = 2
+kalkulator.bagi()
+console.log(kalkulator.hasil) // 3
